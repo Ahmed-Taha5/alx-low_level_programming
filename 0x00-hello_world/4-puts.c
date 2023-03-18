95f27c6a7fc779ead153aa4c6bd2fa5e258e938c
Ahmed-Taha5 <ahmedtaha885@gmail.com>
2023-03-17 17:57:32 -0700

    project 0x00. C - Hello, World Tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Enty point function
+ * Description: print a word with special character
+ * Return: return 0 as main function interger
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print a word using printf
+ * Return: return 0 as main function interger
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return (0);
+}
